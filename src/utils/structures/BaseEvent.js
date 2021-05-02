8f260effb13dd78b1a52918caa6c3d89b3c364f7
Jarco <jarco@familiekers.nl>
2021-05-02 07:55:18 +0200

    A fresh start

--- src/utils/structures/BaseEvent.js
+++
@@ -1,8 +0,0 @@
-module.exports = class BaseEvent {
-    constructor(name) {
-        this.name = name
-        this.utils = require('../Utils');
-        this.config = require('../../config/config.json');
-        this.client = require('../../app');
-    }
-}
